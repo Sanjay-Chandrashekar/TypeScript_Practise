c7e7bc20cb8325c4691375620b8e435b2cbb29dd
Sanjay Chandrashekar <42728799+Sanjay-Chandrashekar@users.noreply.github.com>
2018-08-27 17:58:37 +0530

    Created classVirtualPageTracker.ts file
    Created classVirtualPageTracker.ts file

---
+++ classVirtualPageTracker.ts
@@ -0,0 +1,15 @@
+// class define in TypeScript
+class VirtualPageTracker extends Tracker {
+    private virtualPageName: string = '';
+    constructor(name) {
+        super(name);
+    }
+
+    getName(): void {
+        return this.virtualPageName;
+    }
+
+    static getTrackerName(): string {
+        return  'VirtualPageTracker';
+    }
+}
