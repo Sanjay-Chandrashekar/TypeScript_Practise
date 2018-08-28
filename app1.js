6d5c4d852c3f3092c4a1e84619d59847fece10a6
Sanjay Chandrashekar <42728799+Sanjay-Chandrashekar@users.noreply.github.com>
2018-08-28 17:06:08 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,2 @@
+[0, 1, 2].map(function (num) { return num; });
+//# sourceMappingURL=app1.js.map
---
+++ app1.ts
@@ -0,0 +1,6 @@
+/*
+[0, 1, 2].map(function(num){
+    return num;
+});
+*/
+[0, 1, 2].map((num) => num);
